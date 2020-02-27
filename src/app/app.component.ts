02b55eb9f55e2926c546201d1ad7ad8d031f247f
Johnathan James <haxwell@gmail.com>
2020-02-26 18:24:07 -0700

    Uncommented out the menu icons, added a Home menu item (that still needs work)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,6 +13,11 @@ export class AppComponent implements OnInit {
   public selectedIndex = 0;
   public appPages = [
     {
+      title: 'Home',
+      url: '/folder/Inbox',
+      icon: 'mail'
+    },
+    {
       title: 'List Questions',
       url: '/display',
       icon: 'mail'
