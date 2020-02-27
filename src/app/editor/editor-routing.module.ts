4d457a4882a291806ae91e19a5f54f9358ec3ca9
Johnathan James <haxwell@gmail.com>
2020-02-27 08:54:26 -0700

    Corrected some ui route paths.

--- src/app/editor/editor-routing.module.ts
+++ src/app/editor/editor-routing.module.ts
@@ -7,6 +7,14 @@ const routes: Routes = [
   {
     path: '',
     component: EditorPage
+  },
+  {
+    path: 'tech-profile-topic-edit/:topicId',
+    loadChildren: () => import('./tech-profile-topic-edit/tech-profile-topic-edit.module').then( m => m.TechProfileTopicEditPageModule)
+  },
+  {
+    path: 'tech-profile-line-item-edit/:lineItemId',
+    loadChildren: () => import('./tech-profile-line-item-edit/tech-profile-line-item-edit.module').then( m => m.TechProfileLineItemEditPageModule)
   }
 ];
 
