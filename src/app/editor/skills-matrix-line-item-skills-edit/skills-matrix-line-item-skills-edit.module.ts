0e0e015453b2ce362da89e699514ae2e83a394fc
Johnathan James <haxwell@gmail.com>
2023-05-09 14:19:35 -0600

    Corrected a module imported name

--- src/app/editor/skills-matrix-line-item-skills-edit/skills-matrix-line-item-skills-edit.module.ts
+++ src/app/editor/skills-matrix-line-item-skills-edit/skills-matrix-line-item-skills-edit.module.ts
@@ -7,7 +7,7 @@ import { IonicModule } from '@ionic/angular';
 import { SkillsMatrixLineItemSkillsEditPageRoutingModule } from './skills-matrix-line-item-skills-edit-routing.module';
 
 import { SkillsMatrixLineItemSkillsEditPage } from './skills-matrix-line-item-skills-edit.page';
-import {SavvatoSkillsMatrixLineItemSkillsComponentModule} from "savvato-skills-matrix-line-item-skills-component";
+import {SavvatoSkillsMatrixLineItemSkillsComponentModule} from "@savvato-software/savvato-skills-matrix-line-item-skills-component"
 
 @NgModule({
     imports: [
